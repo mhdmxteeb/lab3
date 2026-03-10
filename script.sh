3032efd5004fb49cca259cb419ad8893a8109e28
mhdmxteeb <ahmedmuteeb46@gmail.com>
2026-03-10 10:20:28 +0530

    Add Jenkins pipeline script for cloning and running

---
+++ script.sh
@@ -0,0 +1,19 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Clone') {
+            steps {
+                git url: 'https://github.com/mithun21-hub/mithu.git',
+                    branch: 'main'
+            }
+        }
+
+        stage('Run Script') {
+            steps {
+                sh 'chmod +x script.sh'
+                sh './script.sh'
+            }
+        }
+    }
+}
